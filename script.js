02510c7acd1a5f1806824a6e95610e9446c74e6d
Misael Randy <152847720+ElMyosotisCode@users.noreply.github.com>
2025-09-02 19:59:03 +0700

    feat(form): enabling to add new task via form submit
    Implements the core logic, so the user can input new task from the form.
    
    - Attaches a 'submit' event listener to the form element.
    - Prevents the default page refresh using event.preventDefault().
    - Captures and trims input values for both title and details.
    - Updates the 'todos' array (source of truth) with the new task object.
    - Re-renders the list and resets the form for a seamless UX.

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 console.log('Script loaded');
 
+// =# STATE & GLOBAL VARIABLES #=
+
 const todos = [
     {
-        id: 1, 
+        id: 1,
+        date_created: '2025-08-30',
         title: 'Learning HTML',
         details: 'Study the basics of HTML including elements, attributes, and semantic tags.',
         completed: true
     },
     {
-        id: 2, 
+        id: 2,
+        date_created: '2025-08-30',
         title: 'Learning CSS', 
         details: 'Understand CSS fundamentals such as selectors, properties, and the box model.',
         completed: true
     },
     {
-        id: 3, 
+        id: 3,
+        date_created: '2025-08-30',
         title: 'Learning JavaScript',
         details: 'Get familiar with JavaScript syntax, data types, and basic programming concepts.',
         completed: false
     },
 ];
 
+// =# ------------------------ #-
+
+// =# DOM ELEMENT SELECTORS #=
+
+const formElement = document.querySelector('#add-todo-form');
+const inputTitleElement = document.querySelector('#input-task-title');
+const inputDetailsElement = document.querySelector('#input-task-details');
 const todoListElement = document.querySelector('#todo-list');
+
+// =# --------------------- #-
+
+// =# FUNCTIONS #=
 
 const renderTodos = () => {
     let todosHTML = '';
@@ -30,13 +46,18 @@ const renderTodos = () => {
         (todo) => { 
             todosHTML += `
                 <li class="todo-app__item ${ todo.completed ? 'todo-completed' : '' }">
+
+                    <div class="todo-app__item-date">
+                        <span class="underline bold">Date created:</span> <br> <span class="italic">${ todo.date_created }</spam>
+                    </div>
+                        
                     <h4 class="todo-app__item-title ${ todo.completed ? 'todo-completed' : '' }">
                         ${ todo.title }
                     </h4>
 
-                    <p class="todo-app__item-details ${ todo.completed ? 'todo-completed' : '' }">
-                        ${ todo.details }
-                    </p>
+                    <p class="todo-app__item-details ${ todo.completed ? 'todo-completed' : '' }">${
+                        todo.details 
+                    }</p>
                 </li>
             `;
         }
@@ -45,4 +66,62 @@ const renderTodos = () => {
     todoListElement.innerHTML = todosHTML;
 }
 
+// =# --------- #=
+
+// =# EVENT LISTENERS #=
+
+formElement.addEventListener(
+    'submit',
+    (event) => {
+        event.preventDefault();
+
+        const inputTitleValue = inputTitleElement.value.trim();
+        const inputDetailsValue = inputDetailsElement.value.trim();
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0];
+
+        // Empty input validation
+
+        if (inputTitleValue === '') {
+            alert('Please enter a task title.');
+            return;
+        }
+
+        else if (inputDetailsValue === '') {
+            alert('Please enter task details.');
+            return;
+        }
+        // ----------------------
+
+        console.log("Form submitted");
+        console.log("Title:", inputTitleValue);
+        console.log("Details:", inputDetailsValue);
+
+        // Insert the input value into a new todo object
+
+        const newTodo = {
+            id: Date.now(), // Unique ID based on timestamp
+            date_created: formattedDate,
+            title: inputTitleValue,
+            details: inputDetailsValue,
+            completed: false
+        }
+
+        todos.push(newTodo);
+        console.log("New Todo Successflly Added:", todos);
+
+        // ---------------------------------------------
+
+        renderTodos();
+        formElement.reset(); // Clear the form inputs
+    }
+);
+
+// =# --------------- #=
+
+
+// =# INITIALIZATION #=
+
 renderTodos();
+
+// =# -------------- #=
